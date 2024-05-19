1564436012e20634b8ed1ebbccd45295ef5d291b
Yeshaya Lazarevich <>
2024-05-19 17:07:47 -0500

    reimplement cascade iteration with VecDequeue

--- src/cascade.rs
+++ src/cascade.rs
@@ -1,105 +1,34 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::{vec_deque::IntoIter, VecDeque},
+    sync::Arc,
+};
 
 use crate::error::GeneratorError;
 
 #[derive(Debug)]
-struct NodeHead<T>(Mutex<Option<NodeRef<T>>>);
+pub struct Cascade<T>(VecDeque<Arc<T>>);
 
-impl<T: Clone> Clone for NodeHead<T> {
-    fn clone(&self) -> Self {
-        Self(Mutex::new(self.0.lock().unwrap().clone())) // todo: don't unwrap
+impl<T> Cascade<T> {
+    pub fn new() -> Self {
+        Self(VecDeque::new())
+    }
+
+    pub fn push(&mut self, value: T) {
+        self.0.push_back(Arc::new(value))
+    }
+
+    pub fn branch(&self) -> Self {
+        Self(self.0.clone())
     }
 }
 
-impl<T> NodeHead<T> {
-    fn set(&self, value: NodeRef<T>) -> Result<(), GeneratorError> {
-        let mut mutex = self.0.try_lock()?; // todo: don't unwrap
-        match *mutex {
-            Some(_) => Err(GeneratorError::HeadAlreadySet),
-            None => {
-                mutex.replace(value);
-                Ok(())
-            }
-        }
-    }
+impl<T> IntoIterator for Cascade<T> {
+    type Item = Arc<T>;
 
-    fn empty() -> Self {
-        Self(Mutex::new(None))
-    }
+    type IntoIter = IntoIter<Arc<T>>;
 
-    fn new(head: NodeRef<T>) -> Self {
-        Self(Mutex::new(Some(head)))
-    }
-
-    fn get(&self) -> Option<NodeRef<T>> {
-        self.0.lock().unwrap().clone()
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Node<T> {
-    value: Arc<T>,
-    head: NodeHead<T>,
-}
-
-#[derive(Debug)]
-pub struct NodeRef<T>(Arc<Node<T>>);
-
-impl<T> NodeRef<T> {
-    pub fn new(value: T) -> Self {
-        Self(Arc::new(Node {
-            value: Arc::new(value),
-            head: NodeHead::empty(),
-        }))
-    }
-
-    pub fn push(self, value: T) -> Result<Self, GeneratorError> {
-        Ok(Self(Arc::new(Node {
-            value: Arc::new(value),
-            head: NodeHead::new(self),
-        })))
-    }
-
-    pub fn branch(&self, value: T) -> Result<Self, GeneratorError> {
-        self.clone().push(value)
-    }
-
-    pub fn into_iter(self) -> NodeIterator<T> {
-        NodeIterator {
-            next_node: Some(self),
-        }
-    }
-
-    pub fn data(self) -> Arc<T> {
-        self.0.value.clone()
-    }
-
-    fn get_head(&self) -> Option<Self> {
-        self.0.head.get()
-    }
-}
-
-impl<T> Clone for NodeRef<T> {
-    fn clone(&self) -> Self {
-        NodeRef(self.0.clone())
-    }
-}
-
-pub struct NodeIterator<T> {
-    next_node: Option<NodeRef<T>>,
-}
-
-impl<T> Iterator for NodeIterator<T> {
-    type Item = NodeRef<T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.next_node.is_some() {
-            let node = self.next_node.as_ref().unwrap().clone();
-            self.next_node = node.get_head();
-            return Some(node);
-        } else {
-            return None;
-        }
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
     }
 }
 
@@ -109,69 +38,54 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_branch() {
-        let branch = NodeRef::new("Avrohom".to_string())
-            .push("Yitzhok".to_string())
-            .unwrap()
-            .push("Yaakov".to_string())
-            .unwrap();
-
-        assert_eq!("Yaakov".to_string(), branch.clone().data().as_ref().clone());
-        assert_eq!(
-            "Yitzhok".to_string(),
-            *branch.get_head().unwrap().clone().data()
-        );
-        assert_eq!(
-            "Avrohom".to_string(),
-            *branch.get_head().unwrap().get_head().unwrap().data()
-        );
-    }
-
-    #[test]
     fn iteration() {
-        let result: Result<Vec<Arc<String>>, GeneratorError> = (|| {
-            Ok(NodeRef::new("Avrohom".to_string())
-                .push("Yitzhok".to_string())?
-                .push("Yakov".to_string())?
-                .into_iter()
-                .map(NodeRef::data)
-                .collect())
-        })();
+        let mut cascade = Cascade::new();
+        vec![
+            "Avrohom".to_string(),
+            "Yitzhok".to_string(),
+            "Yaakov".to_string(),
+        ]
+        .into_iter()
+        .for_each(|value| cascade.push(value));
 
-        match result {
-            Ok(names) => {
-                dbg!(names);
-            }
-            Err(err) => assert!(false, "{:?}", err),
-        }
+        let mut result = cascade
+            .into_iter()
+            .map(|value| value.as_ref().clone())
+            .collect::<Vec<String>>();
+
+        assert_eq!("Yaakov".to_string(), result.pop().unwrap());
+        assert_eq!("Avrohom".to_string(), result.pop().unwrap());
+        assert_eq!("Yitzhok".to_string(), result.pop().unwrap());
     }
 
     #[test]
-    fn branching_branch() {
-        let yitzhok = NodeRef::new("Avrohom".to_string())
-            .push("Yitzhok".to_string())
-            .unwrap();
+    fn cascade_with_branches() {
+        let mut cascade = Cascade::new();
 
-        let result: Result<(Vec<Arc<String>>, Vec<Arc<String>>), GeneratorError> = (|| {
-            Ok((
-                yitzhok
-                    .branch("Eisov".to_string())?
-                    .into_iter()
-                    .map(NodeRef::data)
-                    .collect(),
-                yitzhok
-                    .branch("Yakov".to_string())?
-                    .into_iter()
-                    .map(NodeRef::data)
-                    .collect(),
-            ))
-        })();
+        vec!["Avrohom".to_string(), "Yitzhok".to_string()]
+            .into_iter()
+            .for_each(|value| cascade.push(value));
 
-        match result {
-            Ok((eisov, yakov)) => {
-                println!("Eisov: {:?}\nYitzhok: {:?}", eisov, yakov);
-            }
-            Err(err) => assert!(false, "{:?}", err),
-        }
+        let mut result = vec!["Eisov".to_string(), "Yakov".to_string()]
+            .into_iter()
+            .map(|value| {
+                let mut branch = cascade.branch();
+                branch.push(value);
+                branch
+            })
+            .map(|branch| {
+                branch.into_iter().fold(String::new(), |acc, value| {
+                    if acc.is_empty() {
+                        value.as_ref().clone()
+                    } else {
+                        format!("{}, {}", acc, value)
+                    }
+                })
+            })
+            .collect::<Vec<String>>();
+
+        dbg!(&result);
+        assert_eq!("Avrohom, Yitzhok, Yakov".to_string(), result.pop().unwrap());
+        assert_eq!("Avrohom, Yitzhok, Eisov".to_string(), result.pop().unwrap());
     }
 }
